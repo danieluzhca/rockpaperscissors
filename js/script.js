7c1a3390c4b59665449b6f93e5aa9b69dca68f32
danieluzhca <uzhcadaniel@gmail.com>
2019-11-26 23:23:36 +0000

    added java

--- js/script.js
+++ js/script.js
@@ -8,4 +8,22 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
 
+let playerchoice = $("#input").val();
+});
+$("#result").text(playerchoice);
+});
+
+console.log(winner)
+if{
+    ( playerchoice = "scissor" && computerChoice = "rock")
+$("#result").html("loser");
+}
+else if {
+   ( playerchoice ="rock" && computerChoice = "paper")
+   $("#result").html("LOSER!!!!!")
+}
+else if {
+    (playerchoice="paper" && computerChoice ="scissor})
+    
